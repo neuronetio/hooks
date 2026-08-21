dc92b69107c62cd4523435e5feab0dafff899610
neuronetio <neuronet.io@gmail.com>
2026-08-21 12:26:34 +0200

    oxfmt proseWrap option

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "oxfmt";
 
 export default defineConfig({
   printWidth: 120,
+  proseWrap: "always",
   jsdoc: false,
   ignorePatterns: ["**/*.d.ts", "dist"],
   sortImports: true,
